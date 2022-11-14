2606e388c1c1f6d70a324364cef2f8db97a62389
Rachel <racheljhumes@gmail.com>
2022-11-14 16:48:25 -0700

    added more css styling

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// fetch(
-//   `https://www.googleapis.com/books/v1/volumes?q=${encodeURI(
-//     query
-//   )}&key=${API_KEY}`
-// );
+// GET request from our API
 
 fetch(
   `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=hype&apikey=${API_KEY}`
