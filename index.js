768c464c265546832b00863ebaef959aabec023d
Rachel <racheljhumes@gmail.com>
2022-11-15 10:03:05 -0700

    switched APIs to Studio Ghibili

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-// GET request from our API
+// fetch(
+//   `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=hype&apikey=${API_KEY}`
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
 
-fetch(
-  `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=hype&apikey=${API_KEY}`
-)
+const filmBox = document.querySelector("#film-box");
+
+fetch(`https://ghibliapi.herokuapp.com/films`)
   .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+  .then((allFilms) => {
+    allFilms.forEach((oneFilm) => {
+      renderFilms(oneFilm);
+    });
   });
+
+function renderFilms(film) {
+  const filmElement = document.createElement("div");
+
+  filmBox.append(filmElement);
+
+  filmElement.innerHTML = `<h2 class="heading-primary">${film.title}</h2>
+  <img class="img" src=${film.image} />
+  <p class="description">${film.description}</p>
+  <p class="release-date">${film.release_date}`;
+}
